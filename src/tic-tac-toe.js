d0651d4c5c333d81b0d78c1d56ed349fa54e55fa
Yuliya Aleinik <aleinikya@mail.ru>
2021-08-31 02:13:21 +0300

    feat: trying to do task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -2,20 +2,65 @@ class TicTacToe {
     constructor() {
 
     }
+    currentPlayer = 'x';
+    gameField = [
+        [ null, null, null],
+        [ null, null, null],
+        [ null, null, null],
+    ]
 
     getCurrentPlayerSymbol() {
-
+     return this.currentPlayer
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    nextTurn(rowIndex, colIndex) {
+     if(this.gameField[rowIndex][colIndex]) {
+         return;
+     }
+     this.gameField[rowIndex][colIndex] = this.currentPlayer;
+     this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
     }
 
-    isFinished() {
-
+    isFinished(rowIndex, colIndex) {
+        if(rowIndex !== null && colIndex !== null) {
+            return false;
+         }  
+         
     }
 
-    getWinner() {
+    getWinner(rowIndex, colIndex) {
+      function equals3(a, b, c) {
+          return (a==b && b==c && a!==null);
+      }
+
+
+      let winner = null ;
+      for ( rowIndex = 0; rowIndex < 3; rowIndex++) {
+          if (equals3(this.gameField[rowIndex][0], this.gameField[rowIndex][1], this.gameField[rowIndex][2]) ) {
+              console.log(winner)
+              winner = this.gameField[rowIndex][0];
+          }
+      }
+
+      for ( colIndex= 0; colIndex < 3; colIndex++) {
+        if (equals3(this.gameField[0][colIndex],this.gameField[0][colIndex],this.gameField[0][colIndex]) ) {
+           winner = this.gameField[0][colIndex];
+        }
+    }
+
+    if (equals3(this.gameField[0][0],this.gameField[1][1],this.gameField[2][2]) ){
+       winner = this.gameField[0][0];
+    }
+    if (equals3(this.gameField[2][0],this.gameField[1][1],this.gameField[0][2])) {
+        winner = this.gameField[2][0];
+    }
+     
+    if (winner == null) {
+        return null
+    }else {
+    return winner;
+    }
+    
 
     }
 
@@ -28,7 +73,10 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        if (rowIndex > 2 || colIndex > 2 ) {
+            return null
+        }
+             return this.gameField[rowIndex][colIndex];
     }
 }
 
